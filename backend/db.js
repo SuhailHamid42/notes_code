dbdc9aca256e57435a2fdd372b5beb672f5a71bd
Suhail Hamid <suhailhamid71@gmail.com>
2023-11-03 21:04:40 +0530

    bACKEND

---
+++ backend/db.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+mongoose.set('strictQuery', false);
+// const mongoURI = "mongodb://127.0.0.1:27017/new1b?directConnection=true"
+// const mongoURI = "mongodb+srv://suhailhamid608:Gs8EouT9vGN3WBfS@cluster1.us0tjco.mongodb.net/?retryWrites=true&w=majority"
+// const mongoURI = "mongodb+srv://suhailhamid71:a03WgkMJB4IRx1HG@cluster0.1xvndpy.mongodb.net/?retryWrites=true&w=majority"
+// const mongoURI = "mongodb+srv://suhailhamid71:6VL8kmNWQcYAr1sq@cluster0.thj8kip.mongodb.net/?retryWrites=true&w=majority"
+// const mongoURI = "mongodb+srv://suhail:suhail@cluster0.ismtpw2.mongodb.net/?retryWrites=true&w=majority"
+const mongoURI = process.env.MONGO_URI
+
+
+
+
+
+const connectToMongo = () => {
+    mongoose.connect(mongoURI, {
+        useNewUrlParser : true, 
+        // useCreateIndex : true, 
+        // useUnifiedTopology : true, 
+        // useFindAndModify : false
+    })
+    .then(() => console.log("connected successfully"))
+    .catch((err) => {console.error(err);});
+}
+
+module.exports = connectToMongo;  
+
+// mongodb project1 password Gs8EouT9vGN3WBfS and username suhailhamid608
+// mongodb password a03WgkMJB4IRx1HG
+ // new mongoDbtest(project) password 6VL8kmNWQcYAr1sq
+ // password notesmern   dTbyBoECH5bp9OTt
+//  mongodb+srv://suhailhamid71:TbyBoECH5bp9OTt@cluster0.7fbjuxb.mongodb.net/
+
+
+//suhailhamid806        apJPYFty1DB0dEIm
---
+++ backend/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const connectToMongo = require('./db');
+connectToMongo();
+
+const port = process.env.PORT || 5000
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+
+// Available routes
+// app.use('/api/auth', require('./routes/auth'))
+// app.use('/api/notes', require('./routes/notes')) 
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes')) 
+
+
+app.listen(port, () => {
+  console.log(`new1b Backend listening on port ${port}`)
+})
